06356ecd7c830121e03978e4ea187db901808a60
daichinagano <d.nagano1002@gmail.com>
2024-02-02 18:09:44 +0900

    ✨ create transition veiw

--- frontend/app/memo/page.tsx
+++ frontend/app/memo/page.tsx
@@ -1,18 +1,36 @@
 "use client";
 
+import Link from "next/link";
+
 const App = () => {
   // const [memos, setMemos] = useState<MemoType[]>([]);
   // useEffect(() => {
   //   getMemos().then((response) => setMemos(response.data));
   // }, []);
   return (
-    <div className="flex pt-10 items-center justify-center h-screen bg-gray-200 text-black md:w-full">
+    <div className="flex pt-10 justify-center h-screen bg-gray-200 text-black md:w-full">
       <div className="flex flex-col w-1/2">
+        <Link href="/">
+          <svg
+            className="h-8 w-8 text-black mb-5 ml-40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            {" "}
+            <circle cx="12" cy="12" r="10" />{" "}
+            <polyline points="12 8 8 12 12 16" />{" "}
+            <line x1="16" y1="12" x2="8" y2="12" />
+          </svg>
+        </Link>
         <div>
           <form className="max-w-sm mx-auto">
             <div className="mb-5">
               <label className="block mb-2 text-sm font-medium text-gray-900">
-                title
+                TITLE
               </label>
               <input
                 type="text"
@@ -22,7 +40,7 @@ const App = () => {
             </div>
             <div className="mb-5">
               <label className="block mb-2 text-sm font-medium text-gray-900">
-                input
+                CONTENT
               </label>
               <textarea
                 id="message"
@@ -32,6 +50,44 @@ const App = () => {
               ></textarea>
             </div>
           </form>
+        </div>
+        <div className="flex flex-row-reverse">
+          <button>
+            <svg
+              className="h-12 w-12 text-black mr-40"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <line x1="22" y1="2" x2="11" y2="13" />{" "}
+              <polygon points="22 2 15 22 11 13 2 9 22 2" />
+            </svg>
+          </button>
+          <button>
+            <svg
+              className="h-12 w-12 text-red-500 mr-5"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path stroke="none" d="M0 0h24v24H0z" />{" "}
+              <line x1="4" y1="7" x2="20" y2="7" />{" "}
+              <line x1="10" y1="11" x2="10" y2="17" />{" "}
+              <line x1="14" y1="11" x2="14" y2="17" />{" "}
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />{" "}
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@
 import { MemoType } from "../types/memo";
 import { useEffect, useState } from "react";
 import { getMemos } from "../api/memo";
+import Link from "next/link";
 
 const App = () => {
   // const [memos, setMemos] = useState<MemoType[]>([]);
@@ -13,27 +14,12 @@ const App = () => {
       <div className="flex flex-col w-1/2">
         <div className="pb-4">
           <div className="text-3xl pb-10">Memo</div>
-          <div className="text-xl">過去30日間</div>
           <div className="pb-4">
-            <div className="rounded-lg p-8 shadow-2xl bg-white">
-              <div>test 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="pb-4">
-          <div className="text-xl">2023年</div>
-          <div className="pb-4">
-            <div className="rounded-lg p-8 shadow-2xl bg-white">
-              <div>test 2</div>
-            </div>
-          </div>
-        </div>
-        <div className="pb-4">
-          <div className="text-xl">2022年</div>
-          <div className="pb-4">
-            <div className="rounded-lg p-8 shadow-2xl bg-white">
-              <div>test 3</div>
-            </div>
+            <Link href={"/memo"}>
+              <div className="rounded-lg p-8 shadow-2xl bg-white">
+                <div>test 1</div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
